606c4625ffbf05ae7390f222f41a535c0c507444
Ahmed Eldeep <ahmedeldeep@msn.com>
2020-12-19 23:41:38 +0100

    L08_stm32_examples

---
+++ stm32_examples/src/main.c
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * @file    main.c
+ * @author  Ahmed Eldeep
+ * @email   ahmed@almohandes.org
+ * @website http://almohandes.org/stm32
+ * @date    21.03.2018
+ *          
+ * @brief   main application called after startup
+ * @note    
+ *
+@verbatim
+Copyright (C) Almohandes.org, 2018
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+@endverbatim
+*******************************************************************************/
+
+/* Includes */
+#include "SysTick.h"
+#include "gpio.h"
+
+/**
+ * @addtogroup stm32_examples
+ * @{
+ */
+
+/**
+ * @defgroup main
+ * @brief
+ * @{
+ */
+
+/**
+ * @defgroup main_private_typedefs
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_defines
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_macros
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_constants
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_variables
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_function_prototypes
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_private_functions
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup main_exported_functions
+ * @{
+ */
+
+/**
+ * @brief   main function
+ * @note
+ * @param   none
+ * @retval  none
+ */
+int main(void)
+{
+  SysTick_Init();
+  GPIO_Init_LED(EVAL_GREEN_LED);
+
+  /* Infinite loop */
+  while(1)
+  {
+    GPIO_TurnON_LED(EVAL_GREEN_LED_BITBAND);
+    SysTick_Delay(300);
+
+    GPIO_TurnOFF_LED(EVAL_GREEN_LED_BITBAND);
+    SysTick_Delay(700);
+  }
+}
+
+/**
+ * @}
+ */
+/**
+ * @}
+ */
+/**
+ * @}
+ */
---
+++ stm32_examples/src/src/gpio.c
@@ -0,0 +1,210 @@
+/*******************************************************************************
+ * @file    gpio.c
+ * @author  Ahmed Eldeep
+ * @email   ahmed@almohandes.org
+ * @website http://almohandes.org/stm32
+ * @date    25.03.2018
+ *          
+ * @brief   Some examples on how to use STM32 GPIOs
+ * @note    
+ *
+@verbatim
+Copyright (C) Almohandes.org, 2018
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+@endverbatim
+*******************************************************************************/
+
+/* Includes */
+#include "gpio.h"
+
+/**
+ * @addtogroup stm32_examples
+ * @{
+ */
+
+/**
+ * @defgroup gpio
+ * @brief
+ * @{
+ */
+
+/**
+ * @defgroup gpio_private_typedefs
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup gpio_private_defines
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup gpio_private_macros
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup gpio_private_constants
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup gpio_private_variables
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup gpio_private_function_prototypes
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup gpio_private_functions
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup gpio_exported_functions
+ * @{
+ */
+
+/**
+ * @brief   On-board LEDs initialization function
+ * @note    EVAL_GREEN_LED -> PG13
+ * @param   
+ * @retval
+ */
+void GPIO_Init_LED(Led_Type led)
+{
+  /* Check LED type, please */
+  switch (led)
+  {
+    case EVAL_GREEN_LED:
+
+      /* Enable port G clock */
+      RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
+
+      /* Select output mode */
+      GPIOG->MODER |= GPIO_MODER_MODER13_0;
+
+      /* Select output type push-pull */
+      GPIOG->OTYPER &= ~(GPIO_OTYPER_OT_13);
+
+      /* Select output speed very high */
+      GPIOG->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR13_0 | GPIO_OSPEEDER_OSPEEDR13_1);
+
+      /* Enable SYSCFG clock */
+      RCC->APB2ENR |= RCC_APB2LPENR_SYSCFGLPEN;
+
+      /* Enable compensation cell */
+      SYSCFG->CMPCR |= SYSCFG_CMPCR_CMP_PD;
+
+      /* Wait till compensation cell is ready */
+      while((SYSCFG->CMPCR & SYSCFG_CMPCR_READY) != SYSCFG_CMPCR_READY)
+      {
+
+      }
+
+      /* Select no pull */
+      GPIOG->PUPDR &= ~(GPIO_PUPDR_PUPDR13);
+
+      break;
+
+    default:
+      break;
+  }
+}
+
+/**
+ * @brief   Turns on-board LED on
+ * @note    EVAL_GREEN_LED -> PG13
+ * @param   Led_Type led
+ * @retval
+ */
+void GPIO_TurnON_LED(Led_Type led)
+{
+  switch (led)
+  {
+    case EVAL_GREEN_LED:
+      GPIOG->BSRRL = GPIO_BSRR_BS_13;
+      break;
+
+    case EVAL_GREEN_LED_BITBAND:
+      GREEN_LED_BITBAND = 1;
+      break;
+
+    default:
+      break;
+  }
+}
+
+/**
+ * @brief   Turns on-board LED off
+ * @note    EVAL_GREEN_LED -> PG13
+ * @param   Led_Type led
+ * @retval
+ */
+void GPIO_TurnOFF_LED(Led_Type led)
+{
+  switch (led)
+  {
+    case EVAL_GREEN_LED:
+      GPIOG->BSRRH = GPIO_BSRR_BS_13;
+      break;
+
+    case EVAL_GREEN_LED_BITBAND:
+      GREEN_LED_BITBAND = 0;
+      break;
+
+    default:
+      break;
+  }
+}
+
+/**
+ * @}
+ */
+/**
+ * @}
+ */
+/**
+ * @}
+ */
